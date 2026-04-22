438634b9da6311bf7194256c0c29742d90c357f5
Codnixus <27430075-codnixus@users.noreply.gitlab.com>
2026-04-22 15:12:45 +0100

    Fix add command bugs, add unit test

--- src/command/add.rs
+++ src/command/add.rs
@@ -1,17 +1,17 @@
 use std::{
     env,
-    fs::{self, remove_dir, remove_dir_all, remove_file},
-    path::PathBuf,
+    fs::{self, remove_dir_all, remove_file},
+    path::{Path, PathBuf},
     process::Command,
 };
 
-use color_eyre::eyre::{Context, eyre};
+use color_eyre::eyre::{Context, Result, eyre};
 use glob::Pattern;
 use normalize_path::NormalizePath;
 
 use crate::{
     cli::{AddFlags, OpenEditor},
-    command::util::{GLOB_OPTION, PathKind, copy_recursive, is_glob},
+    command::util::{GLOB_OPTION, copy_recursive, is_glob},
     config::Config,
     error::{GlobError, IoError},
     global_config::GlobalConfig,
@@ -24,7 +24,7 @@ pub fn run(
     file: PathBuf,
     destination: PathBuf,
     flags: AddFlags,
-) -> color_eyre::Result<()> {
+) -> Result<()> {
     let destination = if destination.is_absolute() {
         destination
     } else {
@@ -103,28 +103,10 @@ pub fn run(
         })?;
     }
 
-    if file.is_dir_kind() {
-        if flags.force && destination.exists() {
-            if destination.is_dir_kind() {
-                remove_dir_all(&destination).remove_dir_error(&destination)?;
-            } else {
-                remove_file(&destination).remove_file_error(&destination)?;
-            }
-        }
-    } else if let Some(parent) = destination.parent() {
-        if flags.force {
-            if destination.exists() && destination.is_dir_kind() {
-                remove_dir(&destination).remove_dir_error(&destination)?;
-            }
-            let mut current = Some(parent);
-            while let Some(path) = current {
-                if path.exists() && !path.is_dir_kind() {
-                    fs::remove_file(path).remove_file_error(path)?;
-                    break;
-                }
-                current = path.parent();
-            }
-        }
+    if flags.force {
+        remove_obstructions(&destination)?;
+    }
+    if let Some(parent) = destination.parent() {
         fs::create_dir_all(parent).create_dir_error(parent)?;
     }
 
@@ -143,6 +125,28 @@ pub fn run(
     Ok(())
 }
 
+fn remove_obstructions(path: &Path) -> Result<()> {
+    if let Ok(meta) = fs::symlink_metadata(path) {
+        if meta.is_dir() {
+            remove_dir_all(path).remove_dir_error(path)?;
+        } else {
+            remove_file(path).remove_file_error(path)?;
+        }
+    }
+    let mut current = path.parent();
+    while let Some(dir) = current {
+        if let Ok(meta) = fs::symlink_metadata(dir) {
+            if meta.is_dir() {
+                break;
+            } else {
+                remove_file(dir).remove_file_error(dir)?;
+            }
+        }
+        current = dir.parent();
+    }
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use std::{cell::RefCell, os::unix::fs as unix_fs, path::Path};
@@ -153,82 +157,66 @@ mod tests {
     use tempfile::TempDir;
     use test_case::test_case;
 
+    // -- simple file moves (destination path variations) --
+    #[test_case(|t, _| {
+        fs::write(t.join("file"), "").unwrap();
+        (t.join("file"), "file".into())
+    }, |s, _| {
+        assert!(s.join("file").exists());
+    }; "move_relative_path")]
     #[test_case(|t, s| {
         fs::write(t.join("file"), "").unwrap();
         (t.join("file"), s.join("file"))
-    }, |s| {
+    }, |s, t| {
+        assert!(!t.join("file").exists());
         assert!(s.join("file").exists());
     }; "move_absolute_path")]
     #[test_case(|t, _| {
         fs::write(t.join("file"), "").unwrap();
-        (t.join("file"), "file".into())
-    }, |s| {
-        assert!(s.join("file").exists());
-    }; "move_relative_path")]
-    #[test_case(|t, _| {
-        fs::write(t.join("file"), "").unwrap();
         (t.join("file"), "./dir/../file".into())
-    }, |s| {
+    }, |s, _| {
         assert!(s.join("file").exists());
     }; "move_normalized_path")]
+    // -- source type variations --
     #[test_case(|t, s| {
-        (t.join("file"), s.join("file"))
-    }, |_| {} => panics "move"; "fail_missing_source")]
+        fs::write(t.join("real"), "data").unwrap();
+        unix_fs::symlink(t.join("real"), t.join("link")).unwrap();
+        (t.join("link"), s.join("link"))
+    }, |s, t| {
+        assert!(s.join("link").is_symlink());
+        assert_eq!(
+            fs::read_link(s.join("link")).unwrap(),
+            t.join("real")
+        );
+    }; "move_symlink")]
+    #[test_case(|t, s| {
+        fs::create_dir_all(t.join("dir/sub")).unwrap();
+        fs::write(t.join("dir/sub/file"), "data").unwrap();
+        (t.join("dir"), s.join("dir"))
+    }, |s, _| {
+        assert!(s.join("dir").is_dir());
+        assert!(s.join("dir/sub/file").exists());
+    }; "move_directory")]
+    // -- destination path depth --
+    #[test_case(|t, s| {
+        fs::write(t.join("file"), "data").unwrap();
+        (t.join("file"), s.join("a/b/c/file"))
+    }, |s, _| {
+        assert!(s.join("a/b/c/file").exists());
+    }; "move_to_nested_dest")]
+    // -- failure cases --
+    #[test_case(|_, s| {
+        (s.join("nonexistent"), s.join("dest"))
+    }, |_, _| {} => panics "move"; "fail_missing_source")]
     #[test_case(|t, s| {
         fs::write(t.join("file"), "").unwrap();
         fs::write(s.join("file"), "").unwrap();
         (t.join("file"), s.join("file"))
-    }, |_| {} => panics "already exists"; "fail_dest_exists")]
+    }, |_, _| {} => panics "already exists"; "fail_dest_exists")]
     #[test_case(|t, _| {
         (t.join("file"), "../file".into())
-    }, |_| {} => panics "inside"; "fail_escape_source")]
-    fn test_add(
-        setup: impl FnOnce(&Path, &Path) -> (PathBuf, PathBuf),
-        assertion: impl FnOnce(&Path),
-    ) {
-        let temp_dir = TempDir::new().unwrap();
-        let source_dir = temp_dir.path().join("source");
-        fs::create_dir_all(&source_dir).unwrap();
-        let (f, d) = setup(temp_dir.path(), &source_dir);
-
-        run(
-            source_dir.clone(),
-            None,
-            f,
-            d,
-            AddFlags {
-                copy: false,
-                force: false,
-                editor: None,
-            },
-        )
-        .unwrap();
-
-        assertion(&source_dir);
-    }
-
-    #[test_case(|t, s| {
-        fs::write(t.join("file"), "").unwrap();
-        (t.join("file"), s.join("file"))
-    }, |t, s| {
-        assert!(t.join("file").exists());
-        assert!(s.join("file").exists());
-    }; "1")]
-    #[test_case(|t, s| {
-        unix_fs::symlink(Path::new("/a"), t.join("file")).unwrap();
-        (t.join("file"), s.join("file"))
-    }, |_, s| {
-        assert_eq!(fs::read_link(s.join("file")).unwrap(), Path::new("/a"));
-    }; "2")]
-    #[test_case(|t, s| {
-        fs::create_dir_all(t.join("dir")).unwrap();
-        fs::write(t.join("dir/file"), "").unwrap();
-        (t.join("dir"), s.join("dir"))
-    }, |_, s| {
-        assert!(s.join("dir").exists());
-        assert!(s.join("dir/file").exists());
-    }; "3")]
-    fn test_add_copy(
+    }, |_, _| {} => panics "inside"; "fail_escape_source")]
+    fn test_add_move(
         setup: impl FnOnce(&Path, &Path) -> (PathBuf, PathBuf),
         assertion: impl FnOnce(&Path, &Path),
     ) {
@@ -243,68 +231,157 @@ mod tests {
             f,
             d,
             AddFlags {
-                copy: true,
+                copy: false,
                 force: false,
                 editor: None,
             },
         )
         .unwrap();
 
-        assertion(temp_dir.path(), &source_dir);
+        assertion(&source_dir, temp_dir.path());
     }
 
-    #[test_case(|t, s| {
-        fs::write(t.join("file"), "a").unwrap();
-        fs::write(s.join("file"), "b").unwrap();
-        (t.join("file"), s.join("file"))
-    }, |s| {
-        assert_eq!(fs::read_to_string(s.join("file")).unwrap(), "a");
-    }; "overwrite_existing")]
-    #[test_case(|t, s| {
-        fs::write(t.join("file"), "").unwrap();
-        fs::create_dir_all(s.join("file")).unwrap();
-        (t.join("file"), s.join("file"))
-    }, |s| {
-        assert!(s.join("file").is_file());
-    }; "overwrite_existing_2")]
-    #[test_case(|t, s| {
-        fs::create_dir_all(t.join("dir")).unwrap();
-        fs::write(t.join("dir/file"), "").unwrap();
-        fs::write(s.join("dir"), "").unwrap();
-        (t.join("dir"), s.join("dir"))
-    }, |s| {
-        assert!(s.join("dir/file").exists());
-    }; "overwrite_existing_3")]
-    #[test_case(|t, s| {
-        fs::write(t.join("file"), "").unwrap();
-        fs::write(s.join("dir"), "").unwrap();
-        (t.join("file"), s.join("dir/subdir/file"))
-    }, |s| {
-        assert!(s.join("dir/subdir/file").exists());
-    }; "clear_path_obstruction")]
-    fn test_add_force(
-        setup: impl FnOnce(&Path, &Path) -> (PathBuf, PathBuf),
-        assertion: impl FnOnce(&Path),
-    ) {
-        let temp_dir = TempDir::new().unwrap();
-        let source_dir = temp_dir.path().join("source");
-        fs::create_dir_all(&source_dir).unwrap();
-        let (f, d) = setup(temp_dir.path(), &source_dir);
-
-        run(
-            source_dir.clone(),
-            None,
-            f,
-            d,
-            AddFlags {
-                copy: false,
-                force: true,
-                editor: None,
-            },
-        )
-        .unwrap();
-
-        assertion(&source_dir);
+    #[test_case(
+        |tmp| {
+            fs::write(tmp.join("x"), "data").unwrap();
+            tmp.join("x")
+        },
+        |tmp| {
+            assert!(!tmp.join("x").exists());
+        }
+        ; "file_at_dest"
+    )]
+    #[test_case(
+        |tmp| {
+            fs::create_dir(tmp.join("x")).unwrap();
+            tmp.join("x")
+        },
+        |tmp| {
+            assert!(!tmp.join("x").exists());
+        }
+        ; "empty_dir_at_dest"
+    )]
+    #[test_case(
+        |tmp| {
+            let x = tmp.join("x");
+            fs::create_dir_all(x.join("sub/nested")).unwrap();
+            fs::write(x.join("sub/nested/f"), "data").unwrap();
+            x
+        },
+        |tmp| {
+            assert!(!tmp.join("x").exists());
+        }
+        ; "nonempty_dir_at_dest"
+    )]
+    #[test_case(
+        |tmp| {
+            unix_fs::symlink(Path::new("/nonexistent"), tmp.join("link")).unwrap();
+            tmp.join("link")
+        },
+        |tmp| {
+            assert!(!tmp.join("link").is_symlink());
+        }
+        ; "dangling_symlink_at_dest"
+    )]
+    #[test_case(
+        |tmp| {
+            let real = tmp.join("real");
+            fs::write(&real, "data").unwrap();
+            unix_fs::symlink(&real, tmp.join("link")).unwrap();
+            tmp.join("link")
+        },
+        |tmp| {
+            assert!(!tmp.join("link").is_symlink());
+            assert!(tmp.join("real").exists());
+        }
+        ; "valid_symlink_file_at_dest"
+    )]
+    #[test_case(
+        |tmp| {
+            let real = tmp.join("dir");
+            fs::create_dir(&real).unwrap();
+            fs::write(real.join("f"), "data").unwrap();
+            unix_fs::symlink(&real, tmp.join("link")).unwrap();
+            tmp.join("link")
+        },
+        |tmp| {
+            assert!(!tmp.join("link").is_symlink());
+            assert!(tmp.join("dir/f").exists());
+        }
+        ; "symlink_to_dir_at_dest"
+    )]
+    #[test_case(
+        |tmp| {
+            tmp.join("ghost")
+        },
+        |_| {}
+        ; "nonexistent_dest"
+    )]
+    #[test_case(
+        |tmp| {
+            fs::write(tmp.join("a"), "block").unwrap();
+            tmp.join("a/b/c")
+        },
+        |tmp| {
+            assert!(!tmp.join("a").exists());
+        }
+        ; "file_blocking_parent"
+    )]
+    #[test_case(
+        |tmp| {
+            unix_fs::symlink(Path::new("/nonexistent"), tmp.join("a")).unwrap();
+            tmp.join("a/b/c")
+        },
+        |tmp| {
+            assert!(!tmp.join("a").is_symlink());
+        }
+        ; "dangling_symlink_blocking_parent"
+    )]
+    #[test_case(
+        |tmp| {
+            let real = tmp.join("real");
+            fs::write(&real, "data").unwrap();
+            unix_fs::symlink(&real, tmp.join("a")).unwrap();
+            tmp.join("a/b/c")
+        },
+        |tmp| {
+            assert!(!tmp.join("a").is_symlink());
+            assert!(tmp.join("real").exists());
+        }
+        ; "valid_symlink_blocking_parent"
+    )]
+    #[test_case(
+        |tmp| {
+            fs::create_dir(tmp.join("dir")).unwrap();
+            tmp.join("dir/target")
+        },
+        |tmp| {
+            assert!(tmp.join("dir").is_dir());
+        }
+        ; "no_obstruction_existing_parent"
+    )]
+    #[test_case(
+        |tmp| {
+            tmp.join("a/b/c")
+        },
+        |_| {}
+        ; "no_obstruction_no_parent"
+    )]
+    #[test_case(
+        |tmp| {
+            fs::write(tmp.join("x"), "dest_content").unwrap();
+            tmp.join("x")
+        },
+        |tmp| {
+            assert!(!tmp.join("x").exists());
+        }
+        ; "file_at_dest_clean_parent"
+    )]
+    fn test_remove_obstructions(setup: impl FnOnce(&Path) -> PathBuf, assert: impl FnOnce(&Path)) {
+        let tmp = tempfile::tempdir().unwrap();
+        let target = setup(tmp.path());
+        remove_obstructions(&target).unwrap();
+        assert(tmp.path());
     }
 
     thread_local! {
