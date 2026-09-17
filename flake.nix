a75af99b9bd23ca5a08f9ae131885accfb2a09b3
hexrustox <231717567+hexrustox@users.noreply.github.com>
2026-09-17 12:09:38 +0100

    Update flake inputs

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nix-capsule.url = "gitlab:codnixus/nix-capsule?ref=v0.8.0";
+    nix-capsule.url = "github:hexrustox/nix-capsule?ref=v0.10.3";
   };
 
   outputs =
@@ -28,77 +28,57 @@
           capsule-lib = inputs.nix-capsule.lib { inherit pkgs; };
         in
         {
-          apps.default = {
-            type = "app";
-            program = "${pkgs.ncap}/bin/ncap-direnv";
-          };
-
           devShells = {
             default = capsule-lib.mkShell {
               image = "alpine:latest";
-              devShell = "container";
-              socketPath = "/tmp/dotrift/ncap-socket";
-              containerName = "dotrift";
               extraOptions = [
-                "-e"
-                "HOME"
                 "-e"
                 "CARGO_HOME"
                 "-v"
                 "$CARGO_HOME:$CARGO_HOME"
-                "-v"
-                "$HOME/.cache/pnpm:/root/.cache/pnpm"
               ];
               wrappers = [
                 "cargo"
-                "codebook-lsp"
                 "rust-analyzer"
                 "taplo"
+                "typos"
               ];
               preShellHook = ''
                 export CARGO_HOME=''${CARGO_HOME:-$HOME/.cargo}
+                mkdir -p "$CARGO_HOME"
               '';
             };
 
-            container =
-              let
-                rust = (
-                  pkgs.rust-bin.stable."1.95.0".default.override {
-                    extensions = [
-                      "rust-src"
-                      "rust-analyzer"
-                      "llvm-tools-preview"
-                    ];
-                  }
-                );
-              in
-              pkgs.mkShellNoCC {
-                packages = with pkgs; [
-                  cargo-deny
-                  cargo-edit
-                  cargo-insta
-                  cargo-machete
-                  cargo-llvm-cov
-                  clang
-                  codebook
-                  mold
-                  rust
-                  taplo
+            container = pkgs.mkShellNoCC {
+              packages = with pkgs; [
+                (rust-bin.stable."1.95.0".default.override {
+                  extensions = [
+                    "rust-src"
+                    "rust-analyzer"
+                    "llvm-tools-preview"
+                  ];
+                })
+                cargo-deny
+                cargo-edit
+                cargo-machete
+                cargo-llvm-cov
+                clang
+                mold
 
-                  less
+                taplo
 
-                  skills
-                  git
-                ];
-              };
+                typos
+
+                skills
+                git
+              ];
+            };
           };
         };
 
       systems = [
         "x86_64-linux"
         "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
       ];
     };
 }
